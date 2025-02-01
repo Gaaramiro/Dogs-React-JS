3db2c23af2593fa1b9f61b829b473ffede68d33d
Gaaramiro <gaaramiro@live.com>
2025-02-01 16:01:33 +0100

    Correcao deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: '/Dogs-React-JS/'
+  base: '/Dogs-React-JS'
 });
